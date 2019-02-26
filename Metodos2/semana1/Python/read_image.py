8df8dab035ccdfeae6714940b87877644180f400
Your Name <you@example.com>
2019-02-26 19:39:10 -0300

    C++ adicionado, versao final python

--- Metodos2/semana1/Python/read_image.py
+++ Metodos2/semana1/Python/read_image.py
@@ -1,3 +1,4 @@
+
 from PIL import Image
 
 # Metodo da derivada utilizando a filosofia Forward
@@ -12,14 +13,15 @@ def derivate_backward(fx, fx_backward, delta_x):
 def derivate_central(fx_forward, fx_backward, delta_x):
 	return (fx_forward - fx_backward)/(2*delta_x)
 
-img_open = Image.open('img_open.jpeg') # Imagem a  qual  o objeto vai representar
+img_open = Image.open('verdade.png') # Imagem a  qual  o objeto vai representar
 img_open_mat = img_open.load()  # Carregando a imagem para array
 
 # Criando a imagem de saida
 # new -> cria nova imagem
-# '1' -> 1 bit pixel, ou é branco(1) ou preto(0)
+# '1' -> 1 
+# bit pixel, ou é branco(1) ou preto(0)
 # img_open.size -> imagem é criada com as dimensões de img_open
-img_exit = Image.new('1' , img_open.size) 
+img_exit = Image.new('L' , img_open.size) 
 img_exit_mat = img_exit.load()  # Carregando a imagem array
 
 global resultx, resulty
@@ -43,14 +45,16 @@ for x in range(0, img_open.size[0]-1):
 		else: # pixels em que se aplica a filosofia central
 			resulty = derivate_central(img_open_mat[x+1,y][0], img_open_mat[x, y-1][0], 1)
 
-		# falta comentar
-		treshold = 13
-		if abs(resultx) > treshold:
-			img_exit_mat[x,y] = 1
-		elif abs(resulty) > treshold:
-			img_exit_mat[x,y] = 1
-		else:
-			img_exit_mat[x,y] = 0
+		# # falta comentar
+		# treshold = 40
+		# if abs(resultx) > treshold:
+		#   img_exit_mat[x,y] = 1
+		# elif abs(resulty) > treshold:
+		#   img_exit_mat[x,y] = 1
+		# else:
+		#   img_exit_mat[x,y] = 0
 
+		result =  int(max(abs(resultx), abs(resulty)))
+		img_exit_mat[x,y] = result
 # Salva a imagem saida
 img_exit.save('img_exit.png')
