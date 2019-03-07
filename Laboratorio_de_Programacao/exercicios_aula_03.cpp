55f8c07733960b54acffc4fd55e4226797d96e7b
Your Name <you@example.com>
2019-03-07 14:51:42 -0300

    update

--- Laboratorio_de_Programacao/exercicios_aula_03.cpp
+++ Laboratorio_de_Programacao/exercicios_aula_03.cpp
@@ -3,13 +3,20 @@ using std::cin;using std::cout;
 #include <new>
 using std::nothrow;
 
+void dobrar_vetor(double *v, int tam)
+{
+    double *t = new(nothrow) double[2*tam];
+    for (int i = 0; i != tam; ++i)
+         *t = *(v+i);
+}
+
 double *ler_vetor(int tam)
 {
 	double *v = new(nothrow) double[tam];
 	for (int i = 0; i != tam; ++i)
 	{
-		if(v == nullptr)
-			dobrar_vetor(v);
+		if(v == NULL)
+			dobrar_vetor(v, tam);
 
 		cout << "Digite v["<<i<<"]: ";
 		cin >> v[i];
@@ -18,21 +25,10 @@ double *ler_vetor(int tam)
 	return v;
 }
 
-void dobrar_vetor(double *v, int tam)
-{       
-	double *t = new(nothrow) double[2*tam];
-	for (int i = 0; i != tam; ++i)
-		*t = *(v+i);
-	
-	
-}
-
-int main(){
+void main(){
     int tam = 5;
     double *v = *ler_vetor(tam);
     cout << *v;
     for(int i = 0; i != tam; ++i)
         cout << ", "<<*(v+i);
-
-    return 0;
 }
