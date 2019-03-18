27a08006eacb4d68f53a325fcfaa2ec94ead110b
Your Name <you@example.com>
2019-03-18 16:49:41 -0300

    update quicksort

--- Laboratorio_de_Programacao/quicksort.cpp
+++ Laboratorio_de_Programacao/quicksort.cpp
@@ -1,50 +1,58 @@
 #include <iostream>
-using std::cin;using std::cout;
-using std::nothrow;using std::endl;
+#include <cstdlib>
+#include <ctime>
 #include <new>
+using namespace std;
+
+// generateDouble()
 
 void printVetor(double *v, int tam){
-    for (int i = 0; i < tam; ++i)
-    {
-        cout << "V["<<i<<"] = "<<v[i]<< endl;
-        
-    }
+	for (int i = 0; i < tam; ++i)
+	{
+		cout << "V["<<i<<"] = "<<v[i]<< endl;
+		
+	}
 }
 double *ler_vetor(int tam){
-    double *v = new(nothrow) double(tam);
-    for (int i = 0; i != tam; ++i){
-        cout << "Digite v["<<i<<"]: ";
-        cin >> v[i];
-    }
+	double *v = new(nothrow) double(tam);
+	for (int i = 0; i < tam; ++i){
+		cout << "Digite v["<<i<<"]: ";
+		cin >> v[i];
+	}
 
-    return v;
+	return v;
 }
 
 void swap(double *a, double *b){
-    double aux = *a;
-    *a = *b;
-    *b = *a;
+	double aux = *a;
+	*a = *b;
+	*b = aux;
 }
 
-double *particiona(double *inicio, double *fim, double *pivo){
-    swap(inicio, pivo);
-    double *limite = inicio+1;
-    for (double *i = inicio+1; i != fim; ++i)
-        if(*i <= *inicio)
-            swap(i, limite);
-        limite++;           
+void particiona(double *inicio, double *fim, double *pivo){
+	swap(inicio, pivo);
+	double *limite = inicio+1;
+	for (double *i = inicio+1; i != fim; ++i){
+		if(*i <= *inicio){
+			swap(i, limite);
+			limite++;           
+		}
+	}
+	swap(inicio, limite-1);
 }
 
-double *quicksort(double *inicio, double *fim, int tam){
-    return particiona(inicio, fim, inicio+tam/2);
+void quicksort(double *inicio, double *fim, int tam){
+	particiona(inicio, fim, inicio+tam/2);
 }
 
 int main(){
-    int tam;
-    cout << "Tamanho do vetor: ";
-    cin >> tam;
-    double *v = ler_vetor(tam);
-    printVetor(v, tam);
-    printVetor(v, tam);
-    return 0;
+	int tam;
+	cout << "Tamanho do vetor: ";
+	cin >> tam;
+	double *v = ler_vetor(tam);
+	printVetor(v, tam);
+	particiona(v, v+tam, v);
+	cout << endl;
+	printVetor(v, tam);
+	return 0;
 }
