8bd15d2418f60324ddd8dfcc450f7f0c1aa4cab0
Your Name <you@example.com>
2019-03-18 14:06:55 -0300

    correcao de notacao da alocacao

--- Laboratorio_de_Programacao/exemplo_aula_03.cpp
+++ Laboratorio_de_Programacao/exemplo_aula_03.cpp
@@ -5,45 +5,45 @@ using std::nothrow;
 
 double *ler_vetor(int tam)
 {
-	double *v = new(nothrow) double[tam];
-	if(v != nullptr)
-	{
-		for (int i = 0; i != tam; ++i)
-		{
-			cout << "Digite v["<<i<<"]: ";
-			cin >> v[i];
-		}
-	}
+    double *v = new(nothrow) double(tam);
+    if(v != nullptr)
+    {
+        for (int i = 0; i != tam; ++i)
+        {
+            cout << "Digite v["<<i<<"]: ";
+            cin >> v[i];
+        }
+    }
 
-	return v;
+    return v;
 }
 
 double soma(double *v, int tam)
 {
-	double *fim = v+tam;
-	double sum = 0;
-	for (double *p = v; p != fim; ++p)
-		sum += *p;
-	return sum;
+    double *fim = v+tam;
+    double sum = 0;
+    for (double *p = v; p != fim; ++p)
+        sum += *p;
+    return sum;
 }
 
 int main()
 {
-	int n;
-	do{
-		cout << "TAM: ";
-		cin >> n;
-	}while(n < 1);
+    int n;
+    do{
+        cout << "TAM: ";
+        cin >> n;
+    }while(n < 1);
 
-	double *v = ler_vetor(n);
-	
-	if (v == nullptr)
-	{
-		cout << "sem meória!\n";
-		return 1;
-	}
+    double *v = ler_vetor(n);
+    
+    if (v == nullptr)
+    {
+        cout << "sem meória!\n";
+        return 1;
+    }
 
-	double sum = soma(v,n);
-	cout << "A soma é "<< sum << '\n';
-	delete []v;
+    double sum = soma(v,n);
+    cout << "A soma é "<< sum << '\n';
+    delete []v;
 }
--- Laboratorio_de_Programacao/quicksort.cpp
+++ Laboratorio_de_Programacao/quicksort.cpp
@@ -1,39 +1,50 @@
 #include <iostream>
 using std::cin;using std::cout;
+using std::nothrow;using std::endl;
 #include <new>
-using std::nothrow;
 
+void printVetor(double *v, int tam){
+    for (int i = 0; i < tam; ++i)
+    {
+        cout << "V["<<i<<"] = "<<v[i]<< endl;
+        
+    }
+}
 double *ler_vetor(int tam){
-	double *v = new(nothrow) double[tam];
-	for (int i = 0; i != tam; ++i){
-		cout << "Digite v["<<i<<"]: ";
-		cin >> v[i];
-	{
+    double *v = new(nothrow) double(tam);
+    for (int i = 0; i != tam; ++i){
+        cout << "Digite v["<<i<<"]: ";
+        cin >> v[i];
+    }
 
-	return v;
+    return v;
 }
 
 void swap(double *a, double *b){
-	double aux = *a;
-	*a = *b;
-	*b = *a;
+    double aux = *a;
+    *a = *b;
+    *b = *a;
 }
 
 double *particiona(double *inicio, double *fim, double *pivo){
-	swap(inicio, pivo);
-	double *limite = inicio+1;
-	for (double *i = inicio+1; i != fim; ++i)
-		if(*i <= *inicio)
-			swap(i, limite);
-		limite++;           
+    swap(inicio, pivo);
+    double *limite = inicio+1;
+    for (double *i = inicio+1; i != fim; ++i)
+        if(*i <= *inicio)
+            swap(i, limite);
+        limite++;           
 }
 
 double *quicksort(double *inicio, double *fim, int tam){
-	*inicio = particiona(inicio, fim, inicio+tam/2);
-	
+    return particiona(inicio, fim, inicio+tam/2);
 }
 
 int main(){
-		
-	return 0;
+    int tam;
+    cout << "Tamanho do vetor: ";
+    cin >> tam;
+    double *v = ler_vetor(tam);
+    printVetor(v, tam);
+    printVetor(v, tam);
+    return 0;
 }
